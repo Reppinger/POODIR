48a6665e2bbd200c93071a205244f135f07cb2c9
Randy Eppinger <randy.eppinger@leandog.com>
2016-04-01 21:47:36 -0400

    Added gear_ratio

--- spec/gear_spec.rb
+++ spec/gear_spec.rb
@@ -3,10 +3,13 @@ require_relative '../lib/gear.rb'
 
 RSpec.describe 'Gear' do
 
+  let(:gear) { Gear.new(52, 11, 26, 1.5) }
+
   it 'should calculate ratio' do
-    gear = Gear.new(30,27)
-    expect(gear.ratio).to be_within(0.01).of(1.11)
+    expect(gear.ratio).to be_within(0.01).of(4.72)
   end
 
-
+  it 'should calculate gear_inches' do
+    expect(gear.gear_inches).to be_within(0.01).of(137.09)
+  end
 end
